6c0e19dc7a0727d9af66cb6e3bb79e329ef686a4
Ben Dominguez <ben@fantasyfootballdatapros.com>
2021-02-22 22:54:15 -0500

    updated dates to capital balances, added a loss sale

--- app.py
+++ app.py
@@ -19,11 +19,42 @@ def trades():
             'transaction_time': datetime.utcnow(),
             'sale_amount_minus_commission': 21,
             'average_price_of_asset': 1720
+        }],
+        [{
+            'asset': 'ETH',
+            'side': 'BUY',
+            'transaction_time': datetime.utcnow(),
+            'purchase_amount_minus_commission': 20,
+            'average_price_of_asset': 1700
+        },
+        {
+            'asset': 'ETH',
+            'side': 'SELL',
+            'transaction_time': datetime.utcnow(),
+            'purchase_amount_minus_commission': 19,
+            'average_price_of_asset': 1690
         }]
     ]
     return jsonify(trades)
 
 @app.route('/capital')
 def capital():
-    capital_balances = [20, 21, 22, 23, 24, 25, 26, 27]
+    capital_balances = [
+    {
+        'capital': 20,
+        'utc_time': datetime.utcnow()
+    },
+    {
+        'capital': 21,
+        'utc_time': datetime.utcnow()
+    },
+    {
+        'capital': 22,
+        'utc_time': datetime.utcnow()
+    },
+    {
+        'capital': 23,
+        'utc_time': datetime.utcnow()
+    }
+    ]
     return jsonify(capital_balances)
