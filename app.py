e5de80c1775d4a5b52d0010e9f9896a947f4c026
Ben Dominguez <ben@fantasyfootballdatapros.com>
2021-02-23 09:28:55 -0500

    connected db, added capital balances

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask, jsonify
 from datetime import datetime
+import pymongo
+from dotenv import load_dotenv
+import os
+import ssl
+
+load_dotenv()
+
+PYMONGO_USERNAME = os.getenv('PYMONGO_USERNAME')
+PYMONGO_PASSWORD = os.getenv('PYMONGO_PASSWORD')
+
+PYMONGO_DB_URL = f"mongodb+srv://{PYMONGO_USERNAME}:{PYMONGO_PASSWORD}@trades.cdsd5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(PYMONGO_DB_URL, ssl_cert_reqs=ssl.CERT_NONE)
+db = client.development
+trades = db.trades
+capital = db.capital
 
 app = Flask(__name__)
 
@@ -38,23 +54,13 @@ def trades():
     return jsonify(trades)
 
 @app.route('/capital')
-def capital():
-    capital_balances = [
-    {
-        'capital': 20,
-        'utc_time': datetime.utcnow()
-    },
-    {
-        'capital': 21,
-        'utc_time': datetime.utcnow()
-    },
-    {
-        'capital': 22,
-        'utc_time': datetime.utcnow()
-    },
-    {
-        'capital': 23,
-        'utc_time': datetime.utcnow()
-    }
-    ]
-    return jsonify(capital_balances)
+def show_capital():
+
+    capital_balances = list(capital.find({}))
+
+    json_formatted_capital_balances = []
+    for capital_balance in capital_balances:
+        capital_balance['_id'] = str(capital_balance['_id'])
+        json_formatted_capital_balances.append(capital_balance)
+
+    return jsonify(json_formatted_capital_balances)
